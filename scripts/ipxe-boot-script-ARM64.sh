b8242445297f6de6180b62dbcb85c5dfed50ece4
alexa <alexa@gadopyatikna.mu>
2026-02-28 00:19:32 +0100

    boot script fix no uid

--- scripts/ipxe-boot-script-ARM64.sh
+++ scripts/ipxe-boot-script-ARM64.sh
@@ -5,16 +5,14 @@ dhcp
 
 set server-ip 192.168.0.136
 set target-iqn iqn.2023-10.com.example:storage
-set root-uuid 8e7904a8-848c-4fff-8a8c-75b1098e407b
 
-echo Booting kernel via iSCSI...
+echo Booting Linux over iSCSI...
 
 kernel http://${server-ip}/vmlinuz-6.8.0-31-generic \
     ip=dhcp \
-    root=UUID=${root-uuid} \
-    rootfstype=ext4 \
-    netroot=iscsi:${server-ip}::::${target-iqn} \
-    iscsi_initiator=iqn.2004-10.com.ubuntu:01:33ca4aeade59 \
+    root=/dev/sda2 \
+    iscsi_target_name=${target-iqn} \
+    iscsi_target_ip=${server-ip} \
     rw
 
 initrd http://${server-ip}/initrd.img-6.8.0-31-generic
